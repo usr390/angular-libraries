2ea2e6e46ec7a6b701558669d1fc661baa30d9a3
vendingcompute <edutre1999@gmail.com>
2022-01-25 21:51:56 -0600

    Modified 'card-list.component.ts': added input property that will eventually hold card data

--- projects/ui-controls/src/lib/card-list/card-list.component.ts
+++ projects/ui-controls/src/lib/card-list/card-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Card } from '../card';
 
 @Component({
   selector: 'lib-card-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-list.component.css']
 })
 export class CardListComponent implements OnInit {
+
+  @Input() cards: Card[] = [];
 
   constructor() { }
 
