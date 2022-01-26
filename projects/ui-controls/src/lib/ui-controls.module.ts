bf818f0a0a8e5dec561a9c2d64c0d2172d794b3d
vendingcompute <edutre1999@gmail.com>
2022-01-25 22:03:55 -0600

    Modified 'ui-controls-module.ts': added CommonModule for ngFor functionality

--- projects/ui-controls/src/lib/ui-controls.module.ts
+++ projects/ui-controls/src/lib/ui-controls.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { UiControlsComponent } from './ui-controls.component';
 import { CardListComponent } from './card-list/card-list.component';
-
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
@@ -10,6 +10,7 @@ import { CardListComponent } from './card-list/card-list.component';
     CardListComponent
   ],
   imports: [
+    CommonModule
   ],
   exports: [
     UiControlsComponent,
