28c6a283777d1a5082878c1455a92839bab13ee4
vendingcompute <edutre1999@gmail.com>
2022-01-26 01:16:12 -0600

    Bug fix: changed incorrect import statement preventing proper 'ui-controls' consumption

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { UiControlsComponent } from 'ui-controls';
+import { UiControlsModule } from 'ui-controls';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    UiControlsComponent
+    UiControlsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
