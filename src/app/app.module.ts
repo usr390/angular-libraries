b4e6229d3f85d4aca58c1843d47e240488ca6df7
vendingcompute <edutre1999@gmail.com>
2022-01-27 02:42:20 -0600

    Modified app module: imported forms module. Added to imports array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { UiControlsModule } from 'ui-controls';
 import { AppComponent } from './app.component';
@@ -9,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    UiControlsModule
+    UiControlsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
