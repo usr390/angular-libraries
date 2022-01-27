27b89565bf596472fdb2a11459003396f22d1e20
vendingcompute <edutre1999@gmail.com>
2022-01-27 02:14:23 -0600

    Generated copy button component. Added to 'UI controls' library

---
+++ projects/ui-controls/src/lib/copy-button/copy-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-copy-button',
+  templateUrl: './copy-button.component.html',
+  styleUrls: ['./copy-button.component.css']
+})
+export class CopyButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/ui-controls/src/lib/ui-controls.module.ts
+++ projects/ui-controls/src/lib/ui-controls.module.ts
@@ -3,11 +3,13 @@ import { UiControlsComponent } from './ui-controls.component';
 import { CardListComponent } from './card-list/card-list.component';
 import { CommonModule } from '@angular/common';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CopyButtonComponent } from './copy-button/copy-button.component';
 
 @NgModule({
   declarations: [
     UiControlsComponent,
-    CardListComponent
+    CardListComponent,
+    CopyButtonComponent
   ],
   imports: [
     CommonModule,
@@ -15,7 +17,8 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
   ],
   exports: [
     UiControlsComponent,
-    CardListComponent
+    CardListComponent,
+    CopyButtonComponent
   ]
 })
 export class UiControlsModule { }
